01e6b14a67f10e6eb4d6174cb5e672223419380c
Rick Halterman <halterman@southern.edu>
2021-08-23 21:46:42 -0400

    Version: Mon Aug 23 21:46:41 EDT 2021

---
+++ CopyConstructorPlay/Source.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+class Widget {
+private:
+    int data;
+
+    // Option #1 to prevent copying: Make the copy constructor private 
+    //Widget(const Widget& other) : Widget(other.data) {}
+
+public:
+    Widget(int data) : data(data) {}
+
+    // Normal copy constructor
+    Widget(const Widget& other) : Widget(other.data) {}
+
+    // Option #2 to prevent copying: Make the copy constructor deleted 
+    //Widget(const Widget& other) = delete;
+
+    int get() const {
+        return data;
+    }
+};
+
+int main() {
+    Widget w1{2};
+    std::cout << w1.get() << '\n';
+    // Replace the normal copy constructor with one of the options above to make
+    // the copy construction of w2 illegal
+    Widget w2{ w1 };
+    std::cout << w2.get() << '\n';
+}
